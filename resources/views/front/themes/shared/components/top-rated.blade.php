e1585c8274a86509296150ed4e66fcc257498f4f
Eduardo WB <eduardo@agenciacrow.com.py>
2022-06-07 11:09:48 -0300

    fix top rated product card in theme-01

--- resources/views/front/themes/shared/components/top-rated.blade.php
+++ resources/views/front/themes/shared/components/top-rated.blade.php
@@ -7,9 +7,11 @@
                 <div class="section-top">
                     <h2 class="section-title">
                         {{ __("Top Rated") }}
-                        @if(env('THEME') == "theme-03" || env('THEME') == "theme-05" || env('THEME') == "theme-06" || env('THEME') == "theme-07")
-                        <div id="post-title"> 
-                            <img src="{{ asset('project/resources/views/front/themes/theme-03/assets/images/post-it.png')}}" class="img-fluid" alt="Post it">
+                        @if(env('THEME') == "theme-03" || env('THEME') == "theme-05" || env('THEME') == "theme-06" ||
+                        env('THEME') == "theme-07")
+                        <div id="post-title">
+                            <img src="{{ asset('project/resources/views/front/themes/theme-03/assets/images/post-it.png')}}"
+                                class="img-fluid" alt="Post it">
                         </div>
                         @endif
                     </h2>
@@ -20,11 +22,11 @@
             <div class="col-lg-12 row-theme">
                 <div class="row">
                     @foreach($top_products as $prod)
-                        @if(env('THEME') == "theme-01" || env('THEME') == "theme-02")
-                            @include('includes.product.top-product')
-                        @else
-                            @include('front.themes.'.env('THEME', 'theme-01').'.components.home-product')
-                        @endif
+                    @if(env('THEME','theme-01') == "theme-01" || env('THEME') == "theme-02")
+                    @include('includes.product.top-product')
+                    @else
+                    @include('front.themes.'.env('THEME', 'theme-01').'.components.home-product')
+                    @endif
                     @endforeach
 
                 </div>
