2a8a84fc7c081e4e9147854501bd7fa8bb037e34
Eduardo WB <eduardo@agenciacrow.com.py>
2022-06-07 11:12:31 -0300

    fix big save product card in theme-01

--- resources/views/front/themes/shared/components/big-save.blade.php
+++ resources/views/front/themes/shared/components/big-save.blade.php
@@ -7,9 +7,11 @@
                 <div class="section-top">
                     <h2 class="section-title">
                         {{ __("Big Save") }}
-                        @if(env('THEME') == "theme-03" || env('THEME') == "theme-05" || env('THEME') == "theme-06" || env('THEME') == "theme-07")
-                        <div id="post-title"> 
-                            <img src="{{ asset('project/resources/views/front/themes/theme-03/assets/images/post-it.png')}}" class="img-fluid" alt="Post it">
+                        @if(env('THEME') == "theme-03" || env('THEME') == "theme-05" || env('THEME') == "theme-06" ||
+                        env('THEME') == "theme-07")
+                        <div id="post-title">
+                            <img src="{{ asset('project/resources/views/front/themes/theme-03/assets/images/post-it.png')}}"
+                                class="img-fluid" alt="Post it">
                         </div>
                         @endif
                     </h2>
@@ -25,11 +27,11 @@
                     @endif
                     <div class="row row-theme">
                         @foreach($big_products as $prod)
-                            @if(env('THEME') == "theme-01" || env('THEME') == "theme-02")
-                                @include('includes.product.home-product')
-                            @else 
-                                @include('front.themes.'.env('THEME', 'theme-01').'.components.home-product')
-                            @endif
+                        @if(env('THEME','theme-01') == "theme-01" || env('THEME') == "theme-02")
+                        @include('includes.product.home-product')
+                        @else
+                        @include('front.themes.'.env('THEME', 'theme-01').'.components.home-product')
+                        @endif
                         @endforeach
                     </div>
                 </div>
@@ -37,12 +39,14 @@
                     <div class="aside">
                         @if($ps->big_save_banner)
                         <a class="banner-effect sider-bar-align" href="{{ $ps->big_save_banner_link }}">
-                            <img src="{{asset('assets/images/'.$ps->big_save_banner)}}" alt="" style="width:100%;border-radius: 5px;">
+                            <img src="{{asset('assets/images/'.$ps->big_save_banner)}}" alt=""
+                                style="width:100%;border-radius: 5px;">
                         </a>
                         @endif
                         @if($ps->big_save_banner1)
                         <a class="banner-effect sider-bar-align" href="{{ $ps->big_save_banner_link1 }}">
-                            <img src="{{asset('assets/images/'.$ps->big_save_banner1)}}" alt="" style="width:100%;border-radius: 5px;">
+                            <img src="{{asset('assets/images/'.$ps->big_save_banner1)}}" alt=""
+                                style="width:100%;border-radius: 5px;">
                         </a>
                         @endif
                     </div>
