b1db075cf7081b317bede7b2f1edaa1db68aaa76
Eduardo WB <eduardo@agenciacrow.com.py>
2022-06-17 11:39:21 -0300

    fix loader

--- resources/views/admin/generalsetting/privacypolicy.blade.php
+++ resources/views/admin/generalsetting/privacypolicy.blade.php
@@ -2,92 +2,96 @@
 
 @section('styles')
 <style>
-  .mr-breadcrumb .links .action-list li {
-    display: block;
-  }
+    .mr-breadcrumb .links .action-list li {
+        display: block;
+    }
 
-  .mr-breadcrumb .links .action-list ul {
-    overflow-y: auto;
-    max-height: 240px;
-  }
+    .mr-breadcrumb .links .action-list ul {
+        overflow-y: auto;
+        max-height: 240px;
+    }
 
-  .mr-breadcrumb .links .action-list .go-dropdown-toggle {
-    padding-left: 20px;
-    padding-right: 30px;
-  }
+    .mr-breadcrumb .links .action-list .go-dropdown-toggle {
+        padding-left: 20px;
+        padding-right: 30px;
+    }
 </style>
 @endsection
 
 @section('content')
 
 <div class="content-area">
-  <div class="mr-breadcrumb">
-    <div class="row">
-      <div class="col-lg-12">
-        <h4 class="heading">{{ __("Privacy Policy") }} </h4>
-        <ul class="links">
-          <li>
-            <a href="{{ route('admin.dashboard') }}">{{ __('Dashboard') }} </a>
-          </li>
-          <li>
-            <a href="javascript:;">{{ __("Privacy Policy") }} </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <div class="add-product-content">
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="product-description">
-          <div class="body-area">
-
-            <div class="gocover"
-              style="background: url({{asset('assets/images/'.$gs->admin_loader)}}) no-repeat scroll center center rgba(45, 45, 45, 0.5);">
+    <div class="mr-breadcrumb">
+        <div class="row">
+            <div class="col-lg-12">
+                <h4 class="heading">{{ __("Privacy Policy") }} </h4>
+                <ul class="links">
+                    <li>
+                        <a href="{{ route('admin.dashboard') }}">{{ __('Dashboard') }} </a>
+                    </li>
+                    <li>
+                        <a href="javascript:;">{{ __("Privacy Policy") }} </a>
+                    </li>
+                </ul>
             </div>
-            <form id="geniusform" action="{{ route('admin-gs-update') }}" method="POST" enctype="multipart/form-data">
-              {{ csrf_field() }}
+        </div>
+    </div>
+    <div class="add-product-content">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="product-description">
+                    <div class="body-area">
 
-              @include('includes.admin.form-both')
+                        <div class="gocover"
+                            style="background: url({{asset('storage/images/'.$gs->admin_loader)}}) no-repeat scroll center center rgba(45, 45, 45, 0.5);">
+                        </div>
+                        <form id="geniusform" action="{{ route('admin-gs-update') }}" method="POST"
+                            enctype="multipart/form-data">
+                            {{ csrf_field() }}
 
-              <div class="row">
+                            @include('includes.admin.form-both')
 
-                <div class="col-xl-12">
-                    <div class="input-form">
-                        @component('admin.components.input-localized',["from" => $gs, "type" => "richtext"])
-                        @slot('name')
-                        privacy_policy
-                        @endslot
-                        @slot('value')
-                        privacy_policy
-                        @endslot
-                        {{ __("Privacy Policy") }} <i class="icofont-question-circle" data-toggle="tooltip" style="display: inline-block " data-placement="top" title="{{ __('Text that will appear on the Privacy Policy page') }}"></i>
-                        @endcomponent
+                            <div class="row">
+
+                                <div class="col-xl-12">
+                                    <div class="input-form">
+                                        @component('admin.components.input-localized',["from" => $gs, "type" =>
+                                        "richtext"])
+                                        @slot('name')
+                                        privacy_policy
+                                        @endslot
+                                        @slot('value')
+                                        privacy_policy
+                                        @endslot
+                                        {{ __("Privacy Policy") }} <i class="icofont-question-circle"
+                                            data-toggle="tooltip" style="display: inline-block " data-placement="top"
+                                            title="{{ __('Text that will appear on the Privacy Policy page') }}"></i>
+                                        @endcomponent
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="row justify-content-center">
+
+                                <button class="addProductSubmit-btn" type="submit">{{ __('Save') }}</button>
+
+                            </div>
+
+                        </form>
+
                     </div>
                 </div>
-
-              </div>
-
-              <div class="row justify-content-center">
-            
-                  <button class="addProductSubmit-btn" type="submit">{{ __('Save') }}</button>
-                
-              </div>
-
-            </form>
-
-          </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </div>
 
 @endsection
 
 @section('scripts')
 <script>
-  $('document').ready(function () {
+    $('document').ready(function () {
       $("#store_filter").niceSelect('update');
   });
 
