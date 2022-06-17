3df055ce95feb8c8cf1d925e7ae30868f9adb404
Eduardo WB <eduardo@agenciacrow.com.py>
2022-06-17 10:51:47 -0300

    fix big save banners

--- resources/views/front/themes/shared/components/big-save.blade.php
+++ resources/views/front/themes/shared/components/big-save.blade.php
@@ -10,7 +10,7 @@
                         @if(env('THEME') == "theme-03" || env('THEME') == "theme-05" || env('THEME') == "theme-06" ||
                         env('THEME') == "theme-07")
                         <div id="post-title">
-                            <img src="{{ asset('project/resources/views/front/themes/theme-03/assets/images/post-it.png')}}"
+                            <img src="{{ asset('project/resources/views/front/themes/theme-03/storage/images/post-it.png')}}"
                                 class="img-fluid" alt="Post it">
                         </div>
                         @endif
@@ -39,13 +39,13 @@
                     <div class="aside">
                         @if($ps->big_save_banner)
                         <a class="banner-effect sider-bar-align" href="{{ $ps->big_save_banner_link }}">
-                            <img src="{{asset('assets/images/'.$ps->big_save_banner)}}" alt=""
+                            <img src="{{asset('storage/images/banners/'.$ps->big_save_banner)}}" alt=""
                                 style="width:100%;border-radius: 5px;">
                         </a>
                         @endif
                         @if($ps->big_save_banner1)
                         <a class="banner-effect sider-bar-align" href="{{ $ps->big_save_banner_link1 }}">
-                            <img src="{{asset('assets/images/'.$ps->big_save_banner1)}}" alt=""
+                            <img src="{{asset('storage/images/banners/'.$ps->big_save_banner1)}}" alt=""
                                 style="width:100%;border-radius: 5px;">
                         </a>
                         @endif
