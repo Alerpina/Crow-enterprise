f9c85a9fd63fe5c81776c9d15123e0cca0b963b4
Eduardo WB <eduardo@agenciacrow.com.py>
2022-06-17 12:19:19 -0300

    fix loader

--- resources/views/admin/bank_account/index.blade.php
+++ resources/views/admin/bank_account/index.blade.php
@@ -32,10 +32,13 @@
                         <table id="example" class="table table-hover dt-responsive" cellspacing="0" width="100%">
                             <thead>
                                 <tr>
-                                    <th><i class="icofont-options icofont-lg" data-toggle="tooltip" title='{{ __("Options") }}'></i></th>
+                                    <th><i class="icofont-options icofont-lg" data-toggle="tooltip"
+                                            title='{{ __("Options") }}'></i></th>
                                     <th>{{ __('Name') }}</th>
-                                    <th><i class="fa fa-info-circle fa-lg" data-toggle="tooltip" title='{{ __("Info") }}'></i></th>
-                                    <th><i class="fa fa-eye fa-lg" data-toggle="tooltip" title='{{ __("Status") }}'></i></th>
+                                    <th><i class="fa fa-info-circle fa-lg" data-toggle="tooltip"
+                                            title='{{ __("Info") }}'></i></th>
+                                    <th><i class="fa fa-eye fa-lg" data-toggle="tooltip" title='{{ __("Status") }}'></i>
+                                    </th>
                                 </tr>
                             </thead>
                         </table>
@@ -51,7 +54,7 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="submit-loader">
-                <img src="{{asset('assets/images/'.$admstore->admin_loader)}}" alt="">
+                <img src="{{asset('storage/images/'.$admstore->admin_loader)}}" alt="">
             </div>
             <div class="modal-header">
                 <h5 class="modal-title"></h5>
@@ -129,7 +132,7 @@
         ],
         language: {
             url: '{{$datatable_translation}}',
-            processing: '<img src="{{asset("assets/images/".$admstore->admin_loader)}}">'
+            processing: '<img src="{{asset("storage/images/".$admstore->admin_loader)}}">'
         },
         drawCallback: function(settings) {
             $(this).find('.select').niceSelect();
@@ -151,7 +154,7 @@
                 '<i class="fas fa-plus"></i> <span class="remove-mobile">{{ __("Add New Bank Account") }}<span>' +
                 '</a>' +
                 '</div>');
-            /* 
+            /*
             * Setando no Cookie a página atual
             */
             $("#geniustable").on('page.dt', function(){
@@ -167,8 +170,8 @@
         if(sessionStorage.getItem("CurrentPage") == undefined){
             sessionStorage.setItem("CurrentPage", 0);
         }
-        $(document).on('click', 'a', function(e){ 
-            var link = jQuery(this); 
+        $(document).on('click', 'a', function(e){
+            var link = jQuery(this);
             var x = '{{ Request::route()->getPrefix() }}';
             y = x.split("/");
             if(!(link.attr("data-href") || link.attr("href").indexOf("#") > -1 || link.attr("href").indexOf("javascript") > -1 || link.attr("href").indexOf(y[1]) > -1)){
