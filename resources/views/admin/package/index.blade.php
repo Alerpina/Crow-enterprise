6ef8738cfe1086a7b903776d5bb3dba197e6ecec
Eduardo WB <eduardo@agenciacrow.com.py>
2022-06-17 12:23:27 -0300

    fix loader

--- resources/views/admin/package/index.blade.php
+++ resources/views/admin/package/index.blade.php
@@ -2,73 +2,75 @@
 
 @section('content')
 <div class="content-area">
-  <div class="mr-breadcrumb">
-    <div class="row">
-      <div class="col-lg-12">
-        <h4 class="heading">{{ __('Packagings') }}</h4>
-        <ul class="links">
-          <li>
-            <a href="{{ route('admin.dashboard') }}">{{ __('Dashboard') }} </a>
-          </li>
-          <li>
-            <a href="{{ route('admin-shipping-index') }}">{{ __('Shipping Methods') }}</a>
-          </li>
-          <li>
-            <a href="{{ route('admin-package-index') }}">{{ __('Packagings') }}</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  @include('includes.admin.partials.shipping-tabs')
-  <div class="product-area">
-    <div class="row">
-      <div class="col-lg-12">
-
-        <div class="mr-table allproduct">
-
-          @include('includes.admin.form-success')
-
-          <div class="table-responsiv">
-            <table id="geniustable" class="table table-hover dt-responsive" cellspacing="0" width="100%">
-              <thead>
-                <tr>
-                  <th><i class="icofont-options icofont-lg" data-toggle="tooltip" title='{{ __("Options") }}'></i></th>
-                  <th>{{ __('Title') }}</th>
-                  <th><i class="icofont-dollar icofont-lg" data-toggle="tooltip" title='{{ __("Price") }}'></i></th>
-                </tr>
-              </thead>
-            </table>
-          </div>
+    <div class="mr-breadcrumb">
+        <div class="row">
+            <div class="col-lg-12">
+                <h4 class="heading">{{ __('Packagings') }}</h4>
+                <ul class="links">
+                    <li>
+                        <a href="{{ route('admin.dashboard') }}">{{ __('Dashboard') }} </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('admin-shipping-index') }}">{{ __('Shipping Methods') }}</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('admin-package-index') }}">{{ __('Packagings') }}</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+    @include('includes.admin.partials.shipping-tabs')
+    <div class="product-area">
+        <div class="row">
+            <div class="col-lg-12">
+
+                <div class="mr-table allproduct">
+
+                    @include('includes.admin.form-success')
+
+                    <div class="table-responsiv">
+                        <table id="geniustable" class="table table-hover dt-responsive" cellspacing="0" width="100%">
+                            <thead>
+                                <tr>
+                                    <th><i class="icofont-options icofont-lg" data-toggle="tooltip"
+                                            title='{{ __("Options") }}'></i></th>
+                                    <th>{{ __('Title') }}</th>
+                                    <th><i class="icofont-dollar icofont-lg" data-toggle="tooltip"
+                                            title='{{ __("Price") }}'></i></th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 {{-- ADD / EDIT MODAL --}}
 
 <div class="modal fade" id="modal1" tabindex="-1" role="dialog" aria-labelledby="modal1" aria-hidden="true">
 
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="submit-loader">
-        <img src="{{asset('assets/images/'.$admstore->admin_loader)}}" alt="">
-      </div>
-      <div class="modal-header">
-        <h5 class="modal-title"></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="submit-loader">
+                <img src="{{asset('storage/images/'.$admstore->admin_loader)}}" alt="">
+            </div>
+            <div class="modal-header">
+                <h5 class="modal-title"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
 
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">{{ __('Close') }}</button>
-      </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">{{ __('Close') }}</button>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 
 {{-- ADD / EDIT MODAL ENDS --}}
@@ -76,30 +78,30 @@
 {{-- DELETE MODAL --}}
 
 <div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="modal1" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
+    <div class="modal-dialog">
+        <div class="modal-content">
 
-      <div class="modal-header d-block text-center">
-        <h4 class="modal-title d-inline-block">{{ __('Confirm Delete') }}</h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
+            <div class="modal-header d-block text-center">
+                <h4 class="modal-title d-inline-block">{{ __('Confirm Delete') }}</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
 
-      <!-- Modal body -->
-      <div class="modal-body">
-        <p class="text-center">{{ __('You are about to delete this Packaging.') }}</p>
-        <p class="text-center">{{ __('Do you want to proceed?') }}</p>
-      </div>
+            <!-- Modal body -->
+            <div class="modal-body">
+                <p class="text-center">{{ __('You are about to delete this Packaging.') }}</p>
+                <p class="text-center">{{ __('Do you want to proceed?') }}</p>
+            </div>
 
-      <!-- Modal footer -->
-      <div class="modal-footer justify-content-center">
-        <button type="button" class="btn btn-default" data-dismiss="modal">{{ __('Cancel') }}</button>
-        <a class="btn btn-danger btn-ok">{{ __('Delete') }}</a>
-      </div>
+            <!-- Modal footer -->
+            <div class="modal-footer justify-content-center">
+                <button type="button" class="btn btn-default" data-dismiss="modal">{{ __('Cancel') }}</button>
+                <a class="btn btn-danger btn-ok">{{ __('Delete') }}</a>
+            </div>
 
+        </div>
     </div>
-  </div>
 </div>
 
 {{-- DELETE MODAL ENDS --}}
@@ -111,7 +113,7 @@
 {{-- DATA TABLE --}}
 
 <script type="text/javascript">
-  var table = $('#geniustable').DataTable({
+    var table = $('#geniustable').DataTable({
     stateSave: true,
     stateDuration: -1,
     ordering: false,
@@ -135,7 +137,7 @@
     ],
     language: {
       url: '{{$datatable_translation}}',
-      processing: '<img src="{{asset("assets/images/".$admstore->admin_loader)}}">'
+      processing: '<img src="{{asset("storage/images/".$admstore->admin_loader)}}">'
     },
     drawCallback: function(settings) {
       $(this).find('.select').niceSelect();
@@ -146,7 +148,7 @@
         '<i class="fas fa-plus"></i> {{ __("Add New Packaging") }}' +
         '</a>' +
         '</div>');
-        /* 
+        /*
         * Setando no Cookie a página atual
         */
         $("#geniustable").on('page.dt', function(){
@@ -157,13 +159,13 @@
 </script>
 {{-- DATA TABLE ENDS--}}
 <script>
-  $(document).ready(function(){
+    $(document).ready(function(){
       // First access - CurrentPage
       if(sessionStorage.getItem("CurrentPage") == undefined){
           sessionStorage.setItem("CurrentPage", 0);
       }
-      $(document).on('click', 'a', function(e){ 
-          var link = jQuery(this); 
+      $(document).on('click', 'a', function(e){
+          var link = jQuery(this);
           var x = '{{ Request::route()->getPrefix() }}';
           y = x.split("/");
           if(!(link.attr("data-href") || link.attr("href").indexOf("#") > -1 || link.attr("href").indexOf("javascript") > -1 || link.attr("href").indexOf(y[1]) > -1)){
