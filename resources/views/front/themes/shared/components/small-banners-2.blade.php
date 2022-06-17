d06aee172aa856cb09a2522bda8ae39687232b0e
Eduardo WB <eduardo@agenciacrow.com.py>
2022-06-17 11:12:05 -0300

    fix bottom small banners

--- resources/views/front/themes/shared/components/small-banners-2.blade.php
+++ resources/views/front/themes/shared/components/small-banners-2.blade.php
@@ -2,31 +2,32 @@
 <!-- Banner Area One Start -->
 @if(env("THEME") == "theme-01")
 <section class="banner-section">
-@elseif(env("THEME") == "theme-07" || env("THEME") == "theme-08" )
-<section class="small-banners-2">
-@else 
-<section>
-@endif
-    <div class="container">
-        @foreach($bottom_small_banners->chunk(3) as $chunk)
-        <div class="row {{ env("THEME") == "theme-08" ? "justify-content-center" : ""}}">
-            @foreach($chunk as $img)
-            <div class="col-10 col-lg-4 {{ env("THEME") == "theme-01" || env("THEME") == "theme-02" ? "remove-padding" : ""}}">
-                <div class="left">
-                    @if(env("THEME") == "theme-01" || env("THEME") == "theme-02")
-                    <a class="banner-effect" href="{{ $img->link }}" target="_blank">
-                    @else 
-                    <a class="banner-effect shadow-banner" href="{{ $img->link }}" target="_blank">
-                    @endif
-                        <img src="{{asset('assets/images/banners/'.$img->photo)}}" alt="">
-                    </a>
+    @elseif(env("THEME") == "theme-07" || env("THEME") == "theme-08" )
+    <section class="small-banners-2">
+        @else
+        <section>
+            @endif
+            <div class="container">
+                @foreach($bottom_small_banners->chunk(3) as $chunk)
+                <div class="row {{ env(" THEME")=="theme-08" ? "justify-content-center" : "" }}">
+                    @foreach($chunk as $img)
+                    <div class="col-10 col-lg-4 {{ env(" THEME")=="theme-01" || env("THEME")=="theme-02"
+                        ? "remove-padding" : "" }}">
+                        <div class="left">
+                            @if(env("THEME") == "theme-01" || env("THEME") == "theme-02")
+                            <a class="banner-effect" href="{{ $img->link }}" target="_blank">
+                                @else
+                                <a class="banner-effect shadow-banner" href="{{ $img->link }}" target="_blank">
+                                    @endif
+                                    <img src="{{asset('storage/images/banners/'.$img->photo)}}" alt="">
+                                </a>
+                        </div>
+                    </div>
+                    @endforeach
                 </div>
+                @break
+                @endforeach
             </div>
-            @endforeach
-        </div>
-        @break
-        @endforeach
-    </div>
-</section>
-<!-- Banner Area One Start -->
-@endif
+        </section>
+        <!-- Banner Area One Start -->
+        @endif
