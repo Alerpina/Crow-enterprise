c54dab6a77896eae380e66bd594ae3f177b2bd9d
Eduardo WB <eduardo@agenciacrow.com.py>
2022-06-17 12:39:41 -0300

    fix staff

--- app/Http/Controllers/Admin/StaffController.php
+++ app/Http/Controllers/Admin/StaffController.php
@@ -10,7 +10,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
-
 class StaffController extends Controller
 {
     public function __construct()
@@ -38,7 +37,7 @@ class StaffController extends Controller
                 <div class="godropdown">
                     <button class="go-dropdown-toggle"> ' . __('Actions') . '<i class="fas fa-chevron-down"></i></button>
                     <div class="action-list">
-                        <a data-href="' . route('admin-staff-edit', $data->id) . '" data-header="' . __('Edit Staff') . '" class="edit" data-toggle="modal" data-target="#modal1"> 
+                        <a data-href="' . route('admin-staff-edit', $data->id) . '" data-header="' . __('Edit Staff') . '" class="edit" data-toggle="modal" data-target="#modal1">
                             <i class="fas fa-edit"></i> ' . __('Edit') . '
                         </a>
                         <a href="javascript:;" data-href="' . route('admin-staff-delete', $data->id) . '" data-toggle="modal" data-target="#confirm-delete" class="delete">
@@ -52,7 +51,7 @@ class StaffController extends Controller
     }
 
     //*** GET Request
-  	public function index()
+    public function index()
     {
         return view('admin.staff.index');
     }
@@ -74,11 +73,11 @@ class StaffController extends Controller
                 ];
 
         $validator = Validator::make($request->all(), $rules);
-        
+
         if ($validator->fails()) {
-          return response()->json(array('errors' => $validator->getMessageBag()->toArray()));
+            return response()->json(array('errors' => $validator->getMessageBag()->toArray()));
         }
-        if($request->role_id == 0){
+        if ($request->role_id == 0) {
             return response()->json('Impossible to create user with such role.');
         }
         //--- Validation Section Ends
@@ -86,36 +85,34 @@ class StaffController extends Controller
         //--- Logic Section
         $data = new Admin();
         $input = $request->all();
-        if ($file = $request->file('photo')) 
-         {      
+        if ($file = $request->file('photo')) {
             $name = time().$file->getClientOriginalName();
-            $file->move('assets/images/admins',$name);           
+            $file->move('storage/images/admins', $name);
             $input['photo'] = $name;
-        } 
+        }
         $input['role'] = 'Staff';
         $input['password'] = bcrypt($request['password']);
         $data->fill($input)->save();
         //--- Logic Section Ends
 
-        //--- Redirect Section        
+        //--- Redirect Section
         $msg = __('New Data Added Successfully.');
-        return response()->json($msg);      
-        //--- Redirect Section Ends    
+        return response()->json($msg);
+        //--- Redirect Section Ends
     }
 
 
     public function edit($id)
     {
-        $data = Admin::findOrFail($id);  
+        $data = Admin::findOrFail($id);
         $roles = Role::all();
-        return view('admin.staff.edit',compact('data','roles'));
+        return view('admin.staff.edit', compact('data', 'roles'));
     }
 
-    public function update(Request $request,$id)
+    public function update(Request $request, $id)
     {
         //--- Validation Section
-        if($id != Auth::guard('admin')->user()->id)
-        {
+        if ($id != Auth::guard('admin')->user()->id) {
             $rules =
             [
                 'photo' => 'mimes:jpeg,jpg,png,svg',
@@ -123,76 +120,70 @@ class StaffController extends Controller
             ];
 
             $validator = Validator::make($request->all(), $rules);
-            
+
             if ($validator->fails()) {
-            return response()->json(array('errors' => $validator->getMessageBag()->toArray()));
+                return response()->json(array('errors' => $validator->getMessageBag()->toArray()));
             }
-            if($request->role_id == 0){
+            if ($request->role_id == 0) {
                 return response()->json('Impossible to create user with such role.');
             }
             //--- Validation Section Ends
-            $input = $request->all();  
-            $data = Admin::findOrFail($id);        
-                if ($file = $request->file('photo')) 
-                {              
-                    $name = time().$file->getClientOriginalName();
-                    $file->move('assets/images/admins/',$name);
-                    if($data->photo != null)
-                    {
-                        if (file_exists(public_path().'/assets/images/admins/'.$data->photo)) {
-                            unlink(public_path().'/assets/images/admins/'.$data->photo);
-                        }
-                    }            
+            $input = $request->all();
+            $data = Admin::findOrFail($id);
+            if ($file = $request->file('photo')) {
+                $name = time().$file->getClientOriginalName();
+                $file->move('storage/images/admins/', $name);
+                if ($data->photo != null) {
+                    if (file_exists(public_path().'/storage/images/admins/'.$data->photo)) {
+                        unlink(public_path().'/storage/images/admins/'.$data->photo);
+                    }
+                }
                 $input['photo'] = $name;
-                } 
-            if($request->password == ''){
-                $input['password'] = $data->password;
             }
-            else{
+            if ($request->password == '') {
+                $input['password'] = $data->password;
+            } else {
                 $input['password'] = bcrypt($request->password);
             }
             $data->update($input);
             $msg = __('Data Updated Successfully.');
             return response()->json($msg);
-        }
-        else{
+        } else {
             $msg = 'You can not change your role.';
-            return response()->json($msg);            
+            return response()->json($msg);
         }
- 
     }
 
     //*** GET Request
     public function show($id)
     {
         $data = Admin::findOrFail($id);
-        return view('admin.staff.show',compact('data'));
+        return view('admin.staff.show', compact('data'));
     }
 
     //*** GET Request Delete
     public function destroy($id)
     {
         $data = Admin::findOrFail($id);
-        if($data->role_id == 0)
-    	{
-        return "You don't have access to remove this admin";
-    	}
+        if ($data->role_id == 0) {
+            return "You don't have access to remove this admin";
+        }
         //If Photo Doesn't Exist
-        if($data->photo == null){
+        if ($data->photo == null) {
             $data->delete();
-            //--- Redirect Section     
+            //--- Redirect Section
             $msg = __('Data Deleted Successfully.');
-            return response()->json($msg);      
-            //--- Redirect Section Ends     
+            return response()->json($msg);
+            //--- Redirect Section Ends
         }
         //If Photo Exist
-        if (file_exists(public_path().'/assets/images/admins/'.$data->photo)) {
-            unlink(public_path().'/assets/images/admins/'.$data->photo);
+        if (file_exists(public_path().'/storage/images/admins/'.$data->photo)) {
+            unlink(public_path().'/storage/images/admins/'.$data->photo);
         }
         $data->delete();
-        //--- Redirect Section     
+        //--- Redirect Section
         $msg = __('Data Deleted Successfully.');
-        return response()->json($msg);      
-        //--- Redirect Section Ends    
+        return response()->json($msg);
+        //--- Redirect Section Ends
     }
 }
--- resources/views/admin/staff/index.blade.php
+++ resources/views/admin/staff/index.blade.php
@@ -27,12 +27,16 @@
                         <table id="geniustable" class="table table-hover dt-responsive" cellspacing="0" width="100%">
                             <thead>
                                 <tr>
-                                    <th><i class="icofont-options icofont-lg" data-toggle="tooltip" title='{{ __("Options") }}'></i></th>
+                                    <th><i class="icofont-options icofont-lg" data-toggle="tooltip"
+                                            title='{{ __("Options") }}'></i></th>
                                     <th>{{ __('Name') }}</th>
-                                    <th><i class="icofont-email icofont-lg" data-toggle="tooltip" title='{{ __("Email") }}'></i></th>
-                                    <th><i class="icofont-phone icofont-lg" data-toggle="tooltip" title='{{ __("Phone") }}'></i></th>
+                                    <th><i class="icofont-email icofont-lg" data-toggle="tooltip"
+                                            title='{{ __("Email") }}'></i></th>
+                                    <th><i class="icofont-phone icofont-lg" data-toggle="tooltip"
+                                            title='{{ __("Phone") }}'></i></th>
                                     <th>{{ __('Role') }}</th>
-                                    <th><i class="icofont-calendar icofont-lg" data-toggle="tooltip" title='{{ __("Created at") }}'></i></th>
+                                    <th><i class="icofont-calendar icofont-lg" data-toggle="tooltip"
+                                            title='{{ __("Created at") }}'></i></th>
                                 </tr>
                             </thead>
                         </table>
@@ -50,7 +54,7 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="submit-loader">
-                <img src="{{asset('assets/images/'.$admstore->admin_loader)}}" alt="">
+                <img src="{{asset('storage/images/'.$admstore->admin_loader)}}" alt="">
             </div>
             <div class="modal-header">
                 <h5 class="modal-title"></h5>
@@ -147,7 +151,7 @@
         ],
         language: {
             url: '{{$datatable_translation}}',
-            processing: '<img src="{{asset("assets/images/".$admstore->admin_loader)}}">'
+            processing: '<img src="{{asset("storage/images/".$admstore->admin_loader)}}">'
         },
         drawCallback: function(settings) {
             $(this).find('.select').niceSelect();
@@ -158,7 +162,7 @@
                 '<i class="fas fa-plus"></i> {{ __("Add New Staff") }}' +
                 '</a>' +
                 '</div>');
-            /* 
+            /*
             * Setando no Cookie a página atual
             */
             $("#geniustable").on('page.dt', function(){
@@ -173,8 +177,8 @@
          if(sessionStorage.getItem("CurrentPage") == undefined){
             sessionStorage.setItem("CurrentPage", 0);
         }
-        $(document).on('click', 'a', function(e){ 
-            var link = jQuery(this); 
+        $(document).on('click', 'a', function(e){
+            var link = jQuery(this);
             var x = '{{ Request::route()->getPrefix() }}';
             y = x.split("/");
             if(!(link.attr("data-href") || link.attr("href").indexOf("#") > -1 || link.attr("href").indexOf("javascript") > -1 || link.attr("href").indexOf(y[1]) > -1)){
